132c74a9ac9be6259d614ba3949dcc36bce33a8d
jdmejiav <jdmejiav@eafit.edu.co>
2021-08-23 00:31:56 -0500

    home route modified

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name("home.index");
 
-Auth::routes();
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Auth::routes();
