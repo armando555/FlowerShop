4f8a67a36243cbc12e1123e5918115dd39e3c476
Jriosv <juliianriios@gmail.com>
2021-09-27 23:46:48 -0500

    control panel updated

--- resources/views/adminPanel/panel.blade.php
+++ resources/views/adminPanel/panel.blade.php
@@ -37,7 +37,7 @@
                         @foreach ($flowers as $flower)
                         <li>{{$flower->getName()}}</li>
                         <a class="btn btn-success" href="{{route('flower.show',$flower->getId())}}">{{__('messages.details')}}</a>
-                        <a class="btn btn-danger" href="#">{{__('messages.delete')}}</a>
+                        <a class="btn btn-danger" href="{{route('flower.delete')}}">{{__('messages.delete')}}</a>
                         @endforeach
                     @endif
                 </ul>
@@ -48,7 +48,7 @@
                         @foreach ($bouquets as $bouquet)
                         <li>{{$bouquet->getName()}}</li>
                         <a class="btn btn-success" href="{{route('bouquet.show',$bouquet->getId())}}">{{__('messages.details')}}</a>
-                        <a class="btn btn-danger" href="#">{{__('messages.delete')}}</a>
+                        <a class="btn btn-danger" href="{{route('bouquet.delete')}}">{{__('messages.delete')}}</a>
                         @endforeach
                     @endif
                 </ul>
@@ -59,7 +59,7 @@
                         @foreach ($combos as $combo)
                         <li>{{$combo->getName()}}</li>
                         <a class="btn btn-success" href="{{route('combo.show',$combo->getId())}}">{{__('messages.details')}}</a>
-                        <a class="btn btn-danger" href="#">{{__('messages.delete')}}</a>
+                        <a class="btn btn-danger" href="{{route('combo.delete')}}">{{__('messages.delete')}}</a>
                         @endforeach    
                     @endif
                     
@@ -71,7 +71,7 @@
                         @foreach ($candies as $candy)
                         <li>{{$candy->getName()}}</li>
                         <a class="btn btn-success" href="{{route('candy.show',$candy->getId())}}">{{__('messages.details')}}</a>
-                        <a class="btn btn-danger" href="#">{{__('messages.delete')}}</a>
+                        <a class="btn btn-danger" href="{{route('candy.delete')}}">{{__('messages.delete')}}</a>
                         @endforeach
                     @endif
                     
