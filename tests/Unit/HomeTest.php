997ef29c58e3fac8dd44a19a5a8be53d3cd940b8
jdmejiav <jdmejiav@eafit.edu.co>
2021-11-01 21:47:21 -0500

    Unit testing

---
+++ tests/Unit/HomeTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class HomeTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    /*
+     public function test_home()
+    {
+            
+
+    }*/
+
+}
