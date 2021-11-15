db2798081966f7ab4e5b0f78a2c8ca616e3c840c
Armando Rios Gallego <armandosebas4@gmail.com>
2021-11-15 18:35:41 -0500

    working in xlsx file

--- app/Http/Controllers/Admin/BillController.php
+++ app/Http/Controllers/Admin/BillController.php
@@ -4,21 +4,11 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Interfaces\BillFile;
-use App\Models\Bouquet;
-use App\Models\BouquetFlower;
-use App\Models\Candy;
-use App\Models\Combo;
-use App\Models\Flower;
-use App\Models\User;
 use App\Models\Item;
-use App\Models\Order;
-use PDF;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 use Illuminate\Http\Request;
-use ArielMejiaDev\LarapexCharts\Facades\LarapexChart;
-use Symfony\Component\VarDumper\Cloner\Data;
+use App\Exports\OrdersExport;
+use Maatwebsite\Excel\Facades\Excel;
+
 
 class BillController extends Controller
 {
@@ -34,4 +24,8 @@ class BillController extends Controller
 
         
     }
+    public function generateXlsx($id, Request $request){
+        return Excel::download(new OrdersExport, 'users.xlsx');
+    }
+
 }
